c2c20086fd90f7f6765207bd37edd6bb135e9948
MD Lamim Chowdhury <lamimch777@gmail.com>
2025-05-19 13:24:09 +0600

    Update taskCategoryModel.js

--- backend/Models/taskCategoryModel.js
+++ backend/Models/taskCategoryModel.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+
+const taskCategorySchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['status', 'priority'],
+    required: [true, 'Type is required!']
+  },
+  value: {
+    type: String,
+    required: [true, 'Value is required!']
+  }
+});
+
+const taskCategory = mongoose.model('TaskCategory', taskCategorySchema);
+module.exports = taskCategory;
 
