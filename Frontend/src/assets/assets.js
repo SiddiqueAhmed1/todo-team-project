c3a613eec19c796c17ea787d35c6b885076e1afb
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-05-19 19:01:15 +0600

    create a assets file for use assets

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,9 @@
+import login_image from "./login_image.png";
+import singup_image from "./singup_image.png";
+import user_image from "./user_image.png";
+
+export const assets = {
+  login_image,
+  singup_image,
+  user_image,
+};
