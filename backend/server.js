7291bda4a8acd7ade698d75a6c9a59200318ef32
nshakib <nazmus.shakib.cse@gmail.com>
2025-05-28 11:40:15 +0600

    refac: Refactor auth controller structure for /api/v1/auth

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const limiter = rateLimit({
 
 // Routes
 app.use("/api", limiter); // Apply rate limiting only to specific routes
-app.use("/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/todos", todoRoutes);
 
 
